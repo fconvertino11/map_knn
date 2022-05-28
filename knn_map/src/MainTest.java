5701bdc55b3d03d51e0dcf4f98999713d0d09eec
Francesco <francescoconvertino.ita@gmail.com>
2022-05-28 19:59:12 +0200

    lveewvvwe

--- knn_map/src/MainTest.java
+++ knn_map/src/MainTest.java
@@ -1,7 +1,4 @@
-import data.Data;
-import data.Example;
-import data.ExampleSizeException;
-import data.TrainingDataException;
+import data.*;
 import mining.KNN;
 
 import java.io.FileNotFoundException;
@@ -12,44 +9,42 @@ public class MainTest {
 	 * Classe main in cui effettuare il testing
 	 * @param args Eventuali argomenti che il metodo prende in ingresso
 	 **/
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		try {
 			Data trainingSet= new Data("knn_map/src/simple.dat");
 			
 			KNN knn=new KNN(trainingSet);
-			
 			Example e=new Example(0);
 			e.add("A",0);
 			e.add("B",1);
+
 			try{System.out.println("Prediction with K=1:"+knn.predict(e, 1));
 			} 
 			catch (ExampleSizeException exc) {System.out.println(exc.getMessage());		}
-	
+
 			try{System.out.println("Prediction with K=2:"+knn.predict(e, 2));
 			} 
 			catch (ExampleSizeException exc) {System.out.println(exc.getMessage());		}
-			
+
 			try{System.out.println("Prediction with K=3:"+knn.predict(e, 3));
 			} 
 			catch (ExampleSizeException exc) {System.out.println(exc.getMessage());		}
-			
+
 			try{System.out.println("Prediction with K=4:"+knn.predict(e, 4));
 			} 
 			catch (ExampleSizeException exc) {System.out.println(exc.getMessage());		}
 
 			e=new Example(0);
+
 			e.add("A",0);
 			e.add("B",1);
 
 			System.out.println("Prediction with K=1:" + knn.predict(e, 2));
-
-
 			// read example withKeyboard
-			try{System.out.println("Custom prediction:" + knn.predict());
+			try{
+				System.out.println("Custom prediction:" + knn.predict());
 			}
 			catch (ExampleSizeException exc) {System.out.println(exc.getMessage());		}
-
-
 		}
 		catch(TrainingDataException | ExampleSizeException exc){
 			System.out.println(exc.getMessage());
@@ -58,5 +53,5 @@ public class MainTest {
 			System.out.println("File non trovato");
 		}
 	}
-
 }
+
