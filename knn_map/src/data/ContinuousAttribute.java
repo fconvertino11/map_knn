d9a2b346faea61417a599f6a73beb8505f55f5d3
Francesco <francescoconvertino.ita@gmail.com>
2022-05-28 16:21:22 +0200

    Aggiornata classe ContinuousAttribute.java
    Aggiunti attributi min e max
    Aggiunti metodi setMin e setMax

--- knn_map/src/data/ContinuousAttribute.java
+++ knn_map/src/data/ContinuousAttribute.java
@@ -1,6 +1,22 @@
 package data;
 public class ContinuousAttribute extends Attribute {
+    private double min;
+    private double max;
     ContinuousAttribute(String name, int index) {
         super(name, index);
+    }
+    void setMin(final Double v){
+        if(v < min) {
+            min = v;
+        }
+    }
+
+    public void setMax(final Double m) {
+        if(m > max){
+            max = m;
+        }
+    }
+    double scale (final Double value) {
+        return (value-min)/(max-min);
     }
 }
